1f7488ccf80b37ad0d82223c6c46fc48f7420dc8
SiqueiraKael <crmessi.2004@gmail.com>
2025-04-27 21:30:45 -0300

    commit teste

---
+++ desafio-felipao/index.js
@@ -0,0 +1,32 @@
+// 1° Desafio Classificador de Nível de Herói
+
+// **O que deve ser utilizado**
+
+// - Variáveis 
+// - Operadores 
+// - Laços de repetição
+// - Estrutura de decisões
+
+// Obejetivo - Crie uma variável para armazenar o nome e a quantidade de experência (XP)
+// de um herói, depois utilize uma estrutura de decisão para apresentar alguma mensagem abaixo:
+
+// Se o XP for menor do que 1.000 = Ferro
+// Se o XP estiver entre 1.001 e 2.000 = Bronze
+// Se o XP estiver entre 2.001 e 5.000 = Prata
+// Se o XP estiver entre 6.001 e 7.000 = Ouro
+// Se o XP estiver entre 7.001 e 8.000 = Platina
+// Se o XP estiver entre 8.001 e 9.000 = Ascendente
+// Se o XP estiver entre 9.001 e 10.000 = Imortal 
+// Se o XP for maior ou igual a 10.001 = Radiante
+
+
+
+
+
+
+
+
+
+
+
+
