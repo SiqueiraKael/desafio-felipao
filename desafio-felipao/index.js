80fbe8c3ff92ce00e43130dfa5160a9704f146fe
SiqueiraKael <crmessi.2004@gmail.com>
2025-04-28 01:28:09 -0300

    Sdds python...

--- desafio-felipao/index.js
+++ desafio-felipao/index.js
@@ -8,7 +8,8 @@
 // - Estrutura de decisões
 
 // Obejetivo - Crie uma variável para armazenar o nome e a quantidade de experência (XP)
-// de um herói, depois utilize uma estrutura de decisão para apresentar alguma mensagem abaixo:
+// de um herói, depois utilize uma estrutura de decisão para apresentar alguma mensagem 
+// abaixo:
 
 // Se o XP for menor do que 1.000 = Ferro
 // Se o XP estiver entre 1.001 e 2.000 = Bronze
@@ -19,10 +20,33 @@
 // Se o XP estiver entre 9.001 e 10.000 = Imortal 
 // Se o XP for maior ou igual a 10.001 = Radiante
 
+// Saída - Ao final deve se exibir uma mensagem:
+// "O Herói de nome **{nome}** está no nível de **{nível}**"
 
+let nome = 'João'
+let exp = 10001
 
+let niveis = [
+    {min: 0, max: 1000, nome: 'Ferro'},
+    {min: 1001, max: 2000, nome: 'Bronze'},
+    {min: 2001, max: 5000, nome: 'Prata'},
+    {min: 6001, max: 7000, nome: 'Ouro'},
+    {min: 7001, max: 8000, nome: 'Platina'},
+    {min: 8001, max: 9000, nome: 'Ascendente'},
+    {min: 9001, max: 10000, nome: 'Imortal'},
+    {min: exp>10001, nome: 'Radiante'}
+]
 
+let nivel = 'Radiante'
 
+for (let contador = 0; contador < niveis.length; contador++) {
+    if(exp >= niveis[contador].min && exp <= niveis[contador].max){
+        nivel = niveis[contador].nome
+        break
+    }
+}
+
+console.log(`O herói de nome ${nome} está no nível ${nivel}.`)
 
 
 
